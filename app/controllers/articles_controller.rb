18f9b10028fc5b61f8abcaff69c23bce6c81f2f8
keisuke8248 <keisuke8248@outlook.com>
2020-10-13 03:14:27 +0900

    viewのマークアップ

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_secretKey
+  before_action :set_articleComments
   require 'news-api'
 
   def index
@@ -11,9 +12,7 @@ class ArticlesController < ApplicationController
     category = view_context.categoryArray
     @news = @News.get_top_headlines(country: 'jp',
                                     category: category[params[:id].to_i])
-    @news.each do |n|
-      Article.createArticles(title: n.title, url: n.url, description: n.description, author: n.autohr, publishedAt: n.publishedAt, urlToImage: n.urlToImage)
-    end
+    create_articles
   end
 
   private
@@ -26,6 +25,10 @@ class ArticlesController < ApplicationController
     @news.each do |n|
       Article.createArticles(title: n.title, url: n.url, description: n.description, author: n.author, publishedAt: n.publishedAt, urlToImage: n.urlToImage)
     end
+  end
+
+  def set_articleComments
+    @articleComments = ArticleComment.order('id DESC').limit(15)
   end
 
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -3,8 +3,32 @@ module ArticlesHelper
     ["entertainment", "general", "health", "science", "sports", "technology"]
   end
 
+  def categoryArrayjp
+    ["エンタメ", "一般", "健康", "科学", "スポーツ", "テクノロジー"]
+  end
+
   def articleID(a)
     article = Article.where(title: a.title, url: a.url)
     return article.ids
+  end
+
+  def article_comment(id)
+    comments = ArticleComment.where(article_id: id)
+    case comments
+    when nil
+      return "0"
+    else
+      return comments.length
+    end
+  end
+
+  def articlePath(id)
+    article = Article.find(id)
+    return article_article_comments_path(article.id)
+  end
+
+  def articleTitle(id)
+    article = Article.find(id)
+    return article.title
   end
 end
