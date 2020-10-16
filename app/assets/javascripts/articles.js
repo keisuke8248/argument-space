c76ee1b7da2d9bc2b341b099701e233688364bea
keisuke8248 <keisuke8248@outlook.com>
2020-10-16 18:33:23 +0900

    カテゴリ選択欄を表示のページよって背景色を変化させるよう修正

---
+++ app/assets/javascripts/articles.js
@@ -0,0 +1,22 @@
+$(function(){
+  var url = location.href;
+  console.log(url);
+
+  function addCss (e){
+    e.css('background-color', 'rgb(94, 94, 94)');
+  }
+
+  if (url.match(/\/articles$/)) {
+    let Class = $('.content__top__category#headLine');
+    addCss(Class);
+  } else {
+    for (var i=0; i<=5; i++) {
+      let pattern = new RegExp( "\/articles\/" + i +"$");
+      let result = pattern.test(url);
+      if (result === true) {
+        addCss($(`.content__top__category#${i}`));
+      }
+    }
+  }
+  
+});
