e5df1fa8bd2b863ee294e7bcb119bd818651eeeb
keisuke8248 <keisuke8248@outlook.com>
2020-10-18 18:50:23 +0900

    マイページに高評価率を表示するように実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @evaluations = Evaluation.where(children_user_id: params[:id])
-    @good = @evaluations.where(good: 1)
-    @bad = @evaluations.where(bad: 1)
+    @good = @evaluations.where(good: 1).length
+    @bad = @evaluations.where(bad: 1).length
     @articleComments = ArticleComment.where(user_id: params[:id]).order("created_at DESC").limit(10)
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+
+  def rate(good, bad)
+    denominator = (good + bad)
+    if denominator != 0
+      rate =  (good.to_f / denominator) * 100
+      return rate.round(1)
+    else
+      return "---"
+    end
+  end
+
 end
