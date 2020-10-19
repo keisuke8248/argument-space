e9ffaf26edb89e953872f471f3598af9a9813b4b
keisuke8248 <keisuke8248@outlook.com>
2020-10-19 23:00:41 +0900

    Update 20201001075819_create_articles.rb

--- db/migrate/20201001075819_create_articles.rb
+++ db/migrate/20201001075819_create_articles.rb
@@ -1,12 +1,12 @@
 class CreateArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
-      t.string :title, null: false
-      t.string :url, null: false
+      t.text :title, null: false
+      t.text :url, null: false
       t.text :description
-      t.string :author
-      t.string :publishedAt
-      t.string :urlToImage
+      t.text :author
+      t.text :publishedAt
+      t.text :urlToImage
 
       t.timestamps
     end
