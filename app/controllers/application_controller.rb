b257ff9b5cfd0876734b651824dc5efa647c3527
keisuke8248 <keisuke8248@outlook.com>
2020-11-06 13:05:42 +0900

    新規登録、ログイン画面でヘッダーのレイアウトを変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+  layout :layout_by_resource
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -7,4 +8,14 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
+
+  private
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
+  
 end
